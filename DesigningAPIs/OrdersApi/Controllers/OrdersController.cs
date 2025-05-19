a8625c461002e30d4e47bdebe3a0f5b36f0f369d
Irina Scurtu <scurtuirina@gmail.com>
2025-05-19 16:46:56 +0200

    day1 end

--- DesigningAPIs/OrdersApi/Controllers/OrdersController.cs
+++ DesigningAPIs/OrdersApi/Controllers/OrdersController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OrdersApi.Data.Domain;
+using OrdersApi.Models;
+using OrdersApi.Service.Clients;
+using OrdersApi.Services;
 
 namespace OrdersApi.Controllers
 {
@@ -7,5 +12,44 @@ namespace OrdersApi.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
+        private readonly IOrderService _orderService;
+        private readonly IProductStockServiceClient _productStockServiceClient;
+        private readonly IMapper _mapper;
+
+        public OrdersController(IOrderService orderService,
+            IProductStockServiceClient productStockServiceClient,
+            IMapper mapper)
+        {
+            _orderService = orderService;
+            _productStockServiceClient = productStockServiceClient;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(OrderModel model)
+        {
+            var stocks = await _productStockServiceClient.GetStock(
+                model.OrderItems.Select(p => p.ProductId).ToList());
+
+
+            //To do: Verify stock 
+            var orderToAdd = _mapper.Map<Order>(model);
+            var createdOrder = await _orderService.AddOrderAsync(orderToAdd);
+            // Diminish stock
+
+            return CreatedAtAction("GetOrder", new { id = createdOrder.Id }, createdOrder);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _orderService.GetOrderAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
     }
 }
--- DesigningAPIs/OrdersApi/Program.cs
+++ DesigningAPIs/OrdersApi/Program.cs
@@ -2,7 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using OrdersApi.Data;
 using OrdersApi.Data.Repositories;
+using OrdersApi.Infrastructure;
 using OrdersApi.Service;
+using OrdersApi.Service.Clients;
 using OrdersApi.Services;
 
 namespace OrdersApi
@@ -15,11 +17,24 @@ namespace OrdersApi
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+            });
+
+            builder.Services.AddAutoMapper(typeof(OrderProfileMapping).Assembly);
             builder.Services.AddDbContext<OrderContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            
+
             builder.Services.AddScoped<IOrderRepository, OrderRepository>();
             builder.Services.AddScoped<IOrderService, OrderService>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+           
+            builder.Services.AddHttpClient<IProductStockServiceClient, ProductStockServiceClient>();
+
+
+
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
